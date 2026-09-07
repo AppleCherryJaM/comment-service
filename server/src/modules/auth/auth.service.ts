dd53c73b10e11a66ef5db59f60f5be414297b38e
Max Bolsunovskyi <jamax.cherry@gmail.com>
2026-09-07 17:40:18 +0300

    Updated sql file + added one more user.password check + changed entity fields

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
 
   async login(dto: LoginDto) {
     const user = await this.usersService.findByEmail(dto.email);
-    if (!user) {
+    if (!user || !user.password) {
       throw new UnauthorizedException('Invalid email or password');
     }
 
